5a2f434026856156602e8a377f0aec2540b62f29
binarysudoku <binarysudoku@hotmail.co.uk>
2018-05-01 20:20:17 +0100

    Updated Player Avatar with script references and Player control + movement

--- Unity/FantasyFootball/Assets/Scripts/Player/PlayerControlEngine.cs
+++ Unity/FantasyFootball/Assets/Scripts/Player/PlayerControlEngine.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerControlEngine : MonoBehaviour 
 {
+    //this sucks, will need to be improved when we know what we're doing
+    public GameObject[] spellBook = new GameObject[4]; //contains the spell prefabs the player can use within the match
+
     private bool useGamepad; //should be changed when receiving input from a gamepad / mouse
 
 	// Use this for initialization
--- Unity/FantasyFootball/Assets/Scripts/Player/PlayerMovementEngine.cs
+++ Unity/FantasyFootball/Assets/Scripts/Player/PlayerMovementEngine.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerMovementEngine : MonoBehaviour 
 {
+    private Rigidbody movementRoot; //rigidbody that handles all player movement
+    
 	// Use this for initialization
 	void Start () {
-		
+        movementRoot = transform.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 }
