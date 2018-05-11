5680ab2e1a8ff87b3200c8d1b7b82dfb406ac1f2
binarysudoku <binarysudoku@hotmail.co.uk>
2018-05-11 01:56:23 +0100

    First pass movement code, initial mouse interaction, added biped rig to ArtSource

--- Unity/FantasyFootball/Assets/Scripts/Player/PlayerControlEngine.cs
+++ Unity/FantasyFootball/Assets/Scripts/Player/PlayerControlEngine.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class PlayerControlEngine : MonoBehaviour 
 {
+    public PlayerCameraEngine cameraEngine;
+    public PlayerMovementEngine movementEngine;
+
+    public GameObject controlledBall;
+    
     //this sucks, will need to be improved when we know what we're doing
     public GameObject[] spellBook = new GameObject[4]; //contains the spell prefabs the player can use within the match
 
     private bool useGamepad; //should be changed when receiving input from a gamepad / mouse
+    
+    private Vector3 mouseWorldPosition;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public LayerMask layer_mask;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, layer_mask))
+        {
+            Transform objectHit = hit.transform;
+
+            // Do something with the object that was hit by the raycast.
+            mouseWorldPosition = hit.point;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * 16, Color.yellow);
+        
+        Debug.DrawRay(mouseWorldPosition, Vector3.up*10, Color.yellow);
+
+        movementEngine.moveInput.x = -Input.GetAxis("Horizontal");
+        movementEngine.moveInput.z = Input.GetAxis("Vertical");
+    }
 }
--- Unity/FantasyFootball/Assets/Scripts/Player/PlayerMovementEngine.cs
+++ Unity/FantasyFootball/Assets/Scripts/Player/PlayerMovementEngine.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovementEngine : MonoBehaviour 
 {
-    private Rigidbody movementRoot; //rigidbody that handles all player movement
+    [Header("Physics")]
+    public float timeScale = 1;
+    public float playerMass = 1;
+    public float playerVelocity = 0;
+    public float playerAngular = 0;
+
+    private bool isGrounded = true;
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Vector3 moveInput = new Vector3(0,0,0);
+
+    [Header("Turning")]
+    public float aimAcceleration;
+    private float aimAngle = 0;
+
+    private float aimInput = 0;
+
+    [Header("Tackling")]
+
+    protected Rigidbody movementRoot; //rigidbody that handles all player movement
     
 	// Use this for initialization
 	void Start () {
         movementRoot = transform.GetComponent<Rigidbody>();
-	}
+        
+        //movementRoot.mass *= timeScale;
+        //rigidbody.velocity /= timeScale;
+        //rigidbody.angularVelocity /= timeScale;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        AddMovement(moveInput);
+    }
+    
+    // FixedUpdate is called independently of framerate, all physics should be handled here
+    void FixedUpdate ()
+    {
+        float delta = Time.fixedDeltaTime * timeScale;
+
         
-	}
+
+        //compensates for gravity problems when running in slow/fast motion
+        //movementEngine.movementRoot.velocity += Physics.gravity / rigidbody.mass * delta;
+    }
+    
+    public void AddMovement (Vector3 direction)
+    {
+        movementRoot.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,movementRoot.velocity.y,-Input.GetAxis("Vertical")* moveSpeed);
+        //AddForce(direction * moveAcceleration);
+    }
+
+    public void AdjustPhysicsTimescale()
+    {
+        
+    }
 }
